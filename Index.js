4a75702b360ae0d4c11ddc0befe5584087035580
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-06-16 09:39:25 +0500

    Ready and Deletion , Saving , Searching Features Functional

--- Index.js
+++ Index.js
@@ -1,45 +1,118 @@
-document.addEventListener('click', (Event)=>
-{
+document.addEventListener('click', (Event) => {
 
-    document.querySelector('.Search').style.backgroundColor="white";
-    document.querySelector('.Search').classList.add('Search-Shadow');
+  document.querySelector('.Search').style.backgroundColor = "white";
+  document.querySelector('.Search').classList.add('Search-Shadow');
 
-    const Box = document.getElementById('Search-Text');
-  
-    if (!Box.contains(Event.target)) {
+  const Box = document.getElementById('Search-Text');
 
-      document.querySelector('.Search').classList.remove('Search-Shadow');
-      document.querySelector('.Search').style.backgroundColor="#F1F3F4";
-    }
+  if (!Box.contains(Event.target)) {
+
+    document.querySelector('.Search').classList.remove('Search-Shadow');
+    document.querySelector('.Search').style.backgroundColor = "#F1F3F4";
+  }
 });
 
+function LocalStore() {
+  let Notes = [];
+  let All = document.querySelectorAll('textarea');
+  All.forEach((element) => {
+    if (element.value)
+      return Notes.push(element.value);
+  });
+  localStorage.setItem('Notes', JSON.stringify(Notes));
+}
 
-document.querySelector('.Call').addEventListener('click' , ()=>
-{
+document.querySelector('.Call').addEventListener('click', AddingNote = (Para = ' ') => {
 
-  let NewNote=document.createElement('div');
+  let NewNote = document.createElement('div');
   NewNote.classList.add('Note-Box');
 
-  let HTMLData=
-  `<div class="Operations">
-    <div class="Images">
-        <img class="Edit" src="/Icons/Edit.png" alt="Edit Here">
-        <img class="Del" src="/Icons/Delete.png" alt="Delete Here">
+
+  const HTMLData =
+    `<div class="Operations">
+      <div class="Images">
+          <img class="Save" src="/Icons/Save.png" alt="Save Here">
+          <img class="Del" src="/Icons/Delete.png" alt="Delete Here">
+      </div>
     </div>
-  </div>
-  <p class="Note-Text">
-  </p>
+    <p class="Note-Text Note-Display">
+    </p>
     <textarea name="Write" id="Area" class="Note-Text" cols="100%"></textarea>`
 
-    NewNote.insertAdjacentHTML('afterbegin' , HTMLData);
-    document.querySelector('.All-Notes').appendChild(NewNote);
+  NewNote.insertAdjacentHTML('afterbegin', HTMLData);
+  document.querySelector('.All-Notes').appendChild(NewNote);
 
-    NewNote.querySelector('.Del').addEventListener('click' , ()=>
+
+  NewNote.querySelector('.Del').addEventListener('click', () => {
+    let Ans = confirm('Are You Sure to Delete this Note ? ');
+    if (Ans == true) {
+      NewNote.remove();
+    }
+    LocalStore();
+  });
+
+
+  NewNote.querySelector('.Save').addEventListener('click', () => {
+
+    if (NewNote.querySelector('textarea').value == '') {
+      NewNote.remove();
+      LocalStore();
+    }
+    else {
+      NewNote.querySelector('.Note-Display').innerHTML = NewNote.querySelector('#Area').value;
+      NewNote.querySelector('.Note-Display').style.display = 'block';
+      NewNote.querySelector('#Area').style.setProperty('display', 'none', 'important');
+    }
+    LocalStore();
+  });
+  if (Para != '[object PointerEvent]') {
+    NewNote.querySelector('#Area').innerHTML = Para;
+    NewNote.querySelector('.Note-Display').style.display = "none";
+    NewNote.querySelector('#Area').style.display = "block";
+    LocalStore();
+  }
+  else {
+    NewNote.querySelector('#Area').style.display = "block";
+    NewNote.querySelector('.Note-Display').style.display = "none";
+  }
+});
+
+let GetNotes = JSON.parse(localStorage.getItem('Notes'));
+if (GetNotes) {
+  GetNotes.forEach(Element => {
+    AddingNote(Element);
+  });
+}
+
+// ===================================
+
+document.querySelector('.Search-Click').addEventListener('click' , ()=>
+{
+
+  let Key=document.querySelector('#Search-Text').value;
+  let GetNotes = JSON.parse(localStorage.getItem('Notes'));
+  if (GetNotes) {
+  GetNotes.forEach(Element => {
+    if(Element.search(Key)!=-1)
     {
-      let Ans=confirm('Are You Sure to Delete this Note ? ');
-      if(Ans==true)
-      {
-        NewNote.remove();
+      let All = document.querySelectorAll('textarea');
+      All.forEach((element) => {
+      if (element.value==Element){
+      let Getter=document.querySelector('textarea').parentNode;
+      document.querySelector('.All-Notes').appendChild(Getter);
       }
-    })
+      });
+    }
+    else
+    {
+      let All = document.querySelectorAll('textarea');
+      All.forEach((element) => {
+      if (element.value==Element){
+      let Getter=document.querySelector('textarea').parentNode;
+      document.querySelector('.All-Notes').removeChild(Getter);
+      }
+      });
+    }
+  });
+}
 });
